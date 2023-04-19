9b89d28a4b2ea591966f30e0372f4840dcea0c69
Zoren <zorenleolumosbog@gmail.com>
2023-04-19 23:44:53 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mobile-app-chatting-socket.io',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
